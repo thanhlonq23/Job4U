1edffe7f11f250d1e90411cde105f722b52ca390
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-02-26 01:59:01 +0700

    upload Project API

---
+++ Job4uApi/src/test/java/com/nguyenlonq23/job4uapi/Job4uApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenlonq23.job4uapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4uApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
