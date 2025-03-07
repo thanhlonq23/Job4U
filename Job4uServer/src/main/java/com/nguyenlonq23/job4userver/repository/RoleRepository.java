611cf0ffbc33089b27a428c4b9a149d44f703b2e
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-03-08 03:16:59 +0700

    Update Backend
    - Add and Edit Repo

--- Job4uServer/src/main/java/com/nguyenlonq23/job4userver/repository/RoleRepository.java
+++ Job4uServer/src/main/java/com/nguyenlonq23/job4userver/repository/RoleRepository.java
@@ -1,12 +1,12 @@
 package com.nguyenlonq23.job4userver.repository;
 
-import com.nguyenlonq23.job4userver.model.entity.Status;
+import com.nguyenlonq23.job4userver.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-    Optional<Status> findByName(String name);
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
 }
--- Job4uServer/src/main/java/com/nguyenlonq23/job4userver/repository/UserRepository.java
+++ Job4uServer/src/main/java/com/nguyenlonq23/job4userver/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.nguyenlonq23.job4userver.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
