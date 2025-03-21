d0058cdd81d64f3bb38f0e9786ea47415f0815be
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-03-22 03:30:14 +0700

    Upload Analytics Module

---
+++ Job4uAnalytics/src/main/java/com/nguyenlonq23/job4uanalytics/Job4uAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenlonq23.job4uanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4uAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4uAnalyticsApplication.class, args);
+    }
+
+}
